1681954f550112de5b45170424b98f9f04b35de4
thgus5614@naver.com <thgus5614@naver.com>
2021-06-04 09:22:13 +0900

    Add BaseFragment.kt

---
+++ app/src/main/java/com/sohyun/itunes/ui/base/BaseFragment.kt
@@ -0,0 +1,22 @@
+package com.sohyun.itunes.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+open class BaseFragment<B : ViewDataBinding>(private val resId: Int): Fragment() {
+    protected lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, resId, null, false)
+        return binding.root
+    }
+}
