c604602abf49190cb154efb07bab07e49ff10fa9
leesohyun13 <thgus5614@naver.com>
2021-06-04 18:43:43 +0900

    Add songLocalDataSource on SongRepositoryImpl

--- app/src/main/java/com/sohyun/itunes/data/repository/SongRepository.kt
+++ app/src/main/java/com/sohyun/itunes/data/repository/SongRepository.kt
@@ -1,8 +1,13 @@
 package com.sohyun.itunes.data.repository
 
 import com.sohyun.itunes.data.model.SongResponse
+import com.sohyun.itunes.data.model.Track
 import com.sohyun.itunes.data.network.NetworkStatus
+import kotlinx.coroutines.flow.Flow
 
 interface SongRepository {
     suspend fun searchSong(term: String): NetworkStatus<List<SongResponse.Song>>
+    fun getFavoriteList(): Flow<List<Track>>
+    fun insertItem(track: Track)
+    fun deleteItem(track: Track)
 }
--- app/src/main/java/com/sohyun/itunes/data/repository/SongRepositoryImpl.kt
+++ app/src/main/java/com/sohyun/itunes/data/repository/SongRepositoryImpl.kt
@@ -1,13 +1,30 @@
 package com.sohyun.itunes.data.repository
 
+import com.sohyun.itunes.data.local.SongLocalDataSource
 import com.sohyun.itunes.data.model.SongResponse
+import com.sohyun.itunes.data.model.Track
 import com.sohyun.itunes.data.network.ITunesApi
 import com.sohyun.itunes.data.network.NetworkStatus
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class SongRepositoryImpl @Inject constructor(
-    private val iTunesApi: ITunesApi
+    private val iTunesApi: ITunesApi,
+    private val songLocalDataSource: SongLocalDataSource
 ) : SongRepository, BaseRepository() {
     override suspend fun searchSong(term: String): NetworkStatus<List<SongResponse.Song>> =
         safeApiCall { iTunesApi.searchSong(term).results }
+
+    override fun getFavoriteList(): Flow<List<Track>> = flow {
+        songLocalDataSource.getFavoriteList()
+    }
+
+    override fun insertItem(track: Track) {
+        songLocalDataSource.insertItem(track)
+    }
+
+    override fun deleteItem(track: Track) {
+        songLocalDataSource.deleteItem(track)
+    }
 }
