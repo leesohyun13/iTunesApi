61634c0158704fd7764b57967a8237b167b34ebb
thgus5614@naver.com <thgus5614@naver.com>
2021-06-05 09:15:33 +0900

    Add TrackAdapter and Apply on HomeFragment

--- app/src/main/java/com/sohyun/itunes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sohyun/itunes/ui/home/HomeFragment.kt
@@ -3,19 +3,57 @@ package com.sohyun.itunes.ui.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sohyun.itunes.R
+import com.sohyun.itunes.data.model.Track
 import com.sohyun.itunes.databinding.FragmentHomeBinding
+import com.sohyun.itunes.showToastMessage
 import com.sohyun.itunes.ui.base.BaseFragment
+import com.sohyun.itunes.ui.base.TrackItemListener
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home), TrackItemListener {
     private val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var trackAdapter: TrackAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewmodel = homeViewModel
-        binding.lifecycleOwner = this@HomeFragment
+
+        with(binding) {
+            viewmodel = homeViewModel
+            lifecycleOwner = this@HomeFragment
+
+            homeRecyclerView.run {
+                trackAdapter = TrackAdapter(this@HomeFragment)
+                adapter = trackAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
+            }
+        }
+
+        lifecycleScope.launch { homeViewModel.searchSong() }
+    }
+
+    override fun onClickTrackItem(isFavorite: Boolean, track: Track) {
+        when (isFavorite) {
+            true -> {
+                lifecycleScope.launch(Dispatchers.Default) {
+                    homeViewModel.addFavoriteTrack(track)
+                }
+                showToastMessage(requireContext(), "좋아요 목록에 추가되었습니다.")
+            }
+            false -> {
+                lifecycleScope.launch(Dispatchers.Default) {
+                    homeViewModel.deleteFavoriteTrack(track)
+                }
+                showToastMessage(requireContext(), "좋아요 목록에 삭제되었습니다.")
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/sohyun/itunes/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sohyun/itunes/ui/home/HomeViewModel.kt
@@ -1,14 +1,14 @@
 package com.sohyun.itunes.ui.home
 
-import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.sohyun.itunes.backgroundNotifyObserver
+import com.sohyun.itunes.data.model.SongResponse
 import com.sohyun.itunes.data.model.Track
 import com.sohyun.itunes.data.network.NetworkStatus
 import com.sohyun.itunes.data.repository.SongRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,21 +16,31 @@ class HomeViewModel @Inject constructor(
     private val songRepository: SongRepository
 ): ViewModel() {
     private val isLoading = MutableLiveData(false)
-    private val trackList = MutableLiveData<MutableList<Track>>() // FIXME 페이징 처리를 하려고 MutableList 사용
+    private val songList = MutableLiveData<MutableList<SongResponse.Song>>().apply {
+        value = arrayListOf()
+    }
 
-    private suspend fun searchSong() {
+    suspend fun searchSong() {
         isLoading.value = true
         val response = songRepository.searchSong("greenday")
         when (response) {
             is NetworkStatus.Success -> {
-                Log.d("TAG", "searchSong: ${response.data}")
+                songList.value?.addAll(response.data)
+                songList.backgroundNotifyObserver()
             }
             is NetworkStatus.Failure -> {} // FIXME
         }
         isLoading.value = false
     }
 
-    init {
-        viewModelScope.launch { searchSong() }
+    suspend fun addFavoriteTrack(track: Track) {
+        songRepository.insertItem(track)
     }
+
+    suspend fun deleteFavoriteTrack(track: Track) {
+        songRepository.deleteItem(track)
+    }
+
+    fun getSongList(): LiveData<MutableList<SongResponse.Song>> = songList
+    fun isLoading(): LiveData<Boolean> = isLoading
 }
