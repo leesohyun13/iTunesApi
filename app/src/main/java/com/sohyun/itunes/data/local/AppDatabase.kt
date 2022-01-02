b91b70a1e484b2cb1db4487af84026cc74be6d61
thgus5614@naver.com <thgus5614@naver.com>
2022-01-02 14:09:35 +0900

    라이브러리 버전 수정

--- app/src/main/java/com/sohyun/itunes/data/local/AppDatabase.kt
+++ app/src/main/java/com/sohyun/itunes/data/local/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.sohyun.itunes.data.model.Track
 
-@Database(entities = [Track::class], version = 1)
+@Database(entities = [Track::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun trackDao(): TrackDao
 
